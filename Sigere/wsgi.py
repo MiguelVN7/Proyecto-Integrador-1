9558dd0dc8a725e5c2b1371987f3dd90eaa2ebd8
MiguelVN7 <102312389+MiguelVN7@users.noreply.github.com>
2025-05-07 17:02:40 -0500

    Update wsgi.py

--- Sigere/wsgi.py
+++ Sigere/wsgi.py
@@ -8,8 +8,20 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+import threading
+from django.db import OperationalError
 from django.core.wsgi import get_wsgi_application
+
+def run_populate_script():
+    try:
+        from Sigere.Apps.LandingPage.models import EspacioDeportivo
+        if not EspacioDeportivo.objects.exists():
+            import os
+            os.system("python Sigere/scripts/populate.py")
+    except OperationalError:
+        pass  # Base de datos aún no está lista
+
+threading.Thread(target=run_populate_script).start()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sigere.settings')
 
